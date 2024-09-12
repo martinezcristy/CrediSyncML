b17dbf03451a3b7f7a9086ceb36b545ce211b750
martinezcristy <martinezcristy1221@gmail.com>
2024-09-13 00:05:29 +0800

    Buid members UI: sidebar, table, buttons

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Hello Word pls lang!"
+def members():
+    return render_template('members.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
-    

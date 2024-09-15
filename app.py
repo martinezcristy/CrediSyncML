c610de66a4229b3abd45bd7647650ae3d3c8500b
arvin <arvinsaguisa@gmail.com>
2024-09-15 18:36:32 +0800

    added routes

--- app.py
+++ app.py
@@ -10,5 +10,13 @@ def dashboard():
 def members():
     return render_template('members.html')
 
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
+
+@app.route('/evaluation')
+def evaluation():
+    return render_template('evaluation.html')
+
 if __name__ == "__main__":
     app.run(debug=True)

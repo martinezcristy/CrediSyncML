b3b0e4adb66b5f8efd9012a4e3068f85aa5963b8
Arvin Saguisa <arvinsaguisa@gmail.com>
2024-10-06 11:26:53 +0800

    Revert "form validation"
    This reverts commit d1ec470d9782bd1fdd7eda869ee9ba627cc6955d.

--- app.py
+++ app.py
@@ -1,25 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for
-#import mysql.connector
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-#main route
 @app.route('/')
 def dashboard():
     return render_template('dashboard.html')
 
-#members route
-@app.route('/members', methods=['GET', 'POST'])
+@app.route('/members')
 def members():
-    #sql logic
     return render_template('members.html')
 
-#settings route (edit cooperative profile)
 @app.route('/settings')
 def settings():
     return render_template('settings.html')
 
-#evaluation route
 @app.route('/evaluation')
 def evaluation():
     return render_template('evaluation.html')

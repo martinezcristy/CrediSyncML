0bc0b37f5316a083879f5bf2cf4cb34de0f2d545
martinezcristy <martinezcristy1221@gmail.com>
2024-10-07 23:53:46 +0800

    email & sql set up

--- app.py
+++ app.py
@@ -1,21 +1,53 @@
 from flask import Flask, render_template, request, redirect, url_for
-#import mysql.connector
+#from flask_mysqldb import MYSQL
 
-app = Flask(__name__)
+#from flask_mail import Mail, Message
+# from dotenv import load_dotenv
+# import os
+# load_dotenv()
 
-#main route
-@app.route('/')
+# Create a Blueprint sa email api file
+# email_blueprint = Blueprint('email_api', __name__)
+
+# declare og variables para sa email api (values from env)
+# EMAIL_SERVER_NI_SHA = os.getenv("MAIL_SERVER")
+# EMAIL_PORT_NI_SHA = os.getenv("MAIL_PORT")
+# EMAIL_USE_TLS_NI = os.getenv("MAIL_USE_TLS")
+# EMAIL_USERNAME = os.getenv("MAIL_USERNAME")
+# EMAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+# EMAIL_SENDER_CREDISYNC = os.getenv("MAIL_DEFAULT_SENDER")
+
+app = Flask(__name__) 
+
+# Mail configuration
+# app.config['MAIL_SERVER'] = EMAIL_SERVER_NI_SHA
+# app.config['MAIL_PORT'] = EMAIL_PORT_NI_SHA
+# app.config['MAIL_USE_TLS'] = EMAIL_USE_TLS_NI
+# app.config['MAIL_USERNAME'] = EMAIL_USERNAME
+# app.config['MAIL_PASSWORD'] = EMAIL_PASSWORD
+# app.config['MAIL_DEFAULT_SENDER'] = EMAIL_SENDER_CREDISYNC
+
+#mysql configuration
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = ""
+# app.config['MYSQL_DB'] = ['credisync_db']
+
+# mysql = MYSQL(app)
+
+#dashboard routes
+@app.route('/', methods=['GET'])
 def dashboard():
     return render_template('dashboard.html')
 
 #members route
 @app.route('/members', methods=['GET', 'POST'])
 def members():
-    #sql logic
+    #SQL LOGIC
     return render_template('members.html')
 
-#settings route (edit cooperative profile)
-@app.route('/settings')
+#settings route
+@app.route('/settings', methods=['GET', 'POST'])
 def settings():
     return render_template('settings.html')
 
